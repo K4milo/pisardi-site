e8fcd0fe812bef44c95d6dcabc7f6058f02c56d0
k4milo <kmilo.vanegas@gmail.com>
2019-08-15 06:33:11 -0500

    Fix in general template

--- wp-content/themes/pisardi-theme/includes/general/_content-top.php
+++ wp-content/themes/pisardi-theme/includes/general/_content-top.php
@@ -1,5 +1,9 @@
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
 <section id="ContentPage" class="content-deault">
 	<article class="content-deault__wrapper container">
 		<?php the_content(); ?>
 	</article>
 </section>
+<?php endwhile; ?>
+<?php else: get_template_part('includes/loops/content', 'none'); ?>
+<?php endif; ?>
