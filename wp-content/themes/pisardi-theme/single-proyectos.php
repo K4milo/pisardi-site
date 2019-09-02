2b8c62166bdec7471dfa10bea24f201afe9de527
Camilo Vanegas <Camilo.Vanegas@vml.com>
2019-09-02 17:54:26 -0500

    office work

---
+++ wp-content/themes/pisardi-theme/single-proyectos.php
@@ -0,0 +1,9 @@
+<?php 
+
+get_template_part('includes/header');
+
+    get_template_part('includes/loops/content', 'product'); 
+    get_template_part('includes/loops/content', 'product-slider'); 
+    get_template_part('includes/loops/content', 'product-related'); 
+
+get_template_part('includes/footer'); 
