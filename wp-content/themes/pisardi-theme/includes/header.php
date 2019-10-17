b37818490d8744a1d302cad553e65c04437bc4ca
k4milo <kmilo.vanegas@gmail.com>
2019-10-17 06:43:37 -0500

    Favicon added

--- wp-content/themes/pisardi-theme/includes/header.php
+++ wp-content/themes/pisardi-theme/includes/header.php
@@ -4,7 +4,8 @@
 	<title><?php wp_title();?></title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="<?php bloginfo('template_url');?>/images/favicon.png" />
 	<?php wp_head(); ?>
 </head>
 
